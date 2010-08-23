9e8098e367412dc92e54658f350f8b3db4871059
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2010-08-23 16:55:55 -0300

    Updated for Google App Engine.

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
 (defproject licensator "0.1-SNAPSHOT"
   :description "A small webapp that helps you choose the right license for your open source projects."
   :url "http://github.com/danielfm/licensator"
+  :namespaces [licensator.local
+	       licensator.production]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [compojure "0.4.1"]
 		 [ring "0.2.5"]
 		 [ring/ring-jetty-adapter "0.2.5"]
+		 [ring/ring-servlet "0.2.5"]
 		 [hiccup "0.2.6"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
 		     [lein-run "1.0.0-SNAPSHOT"]]
-  :run-aliases {:server [licensator.run -main]})
+  :compile-path "war/WEB-INF/classes"
+  :library-path "war/WEB-INF/lib"
+  :run-aliases {:server [licensator.local -main]})
